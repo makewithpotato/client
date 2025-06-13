eaa8d7a7843f498bcf368a76c69fbbc253ef3f21
Kimd0ng <kdongh0406@gmail.com>
2025-06-14 01:01:56 +0900

    Feat: update index file to export Modal and ProgramBookTitleModal components

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,5 @@ export * from './Layout';
 export * from './TopBar';
 export * from './ProgramBookCard';
 export * from './SearchBar';
+export * from './Modal';
+export * from './ProgramBookTitleModal';
